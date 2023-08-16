6de92779d9a6a38e4162cdcf60a6dab41b608545
pop <906792895@qq.com>
2023-08-16 14:03:39 +0800

    feat: add mida limit order test

--- packages/sdk/src/midaswap/addresses.ts
+++ packages/sdk/src/midaswap/addresses.ts
@@ -1,9 +1,11 @@
 import { ChainIdToAddress, Network } from "../utils";
 
 export const PairFactory: ChainIdToAddress = {
-  [Network.EthereumGoerli]: "0x1a2a2e817f1ad8b17311a01a4996fcd89773d1e0",
+  // [Network.EthereumGoerli]: "0x1a2a2e817f1ad8b17311a01a4996fcd89773d1e0",
+  [Network.EthereumGoerli]: "0x25488d33e2b86b00D8ed8873F241d84972F46256",
 };
 
 export const Router: ChainIdToAddress = {
-  [Network.EthereumGoerli]: "0xc6a1a2738cfa91c5205bb57e21820b38b65c09a9",
+  // [Network.EthereumGoerli]: "0xc6a1a2738cfa91c5205bb57e21820b38b65c09a9",
+  [Network.EthereumGoerli]: "0x700b61933859503Dac783a65FF30463E208B2e4e",
 };
